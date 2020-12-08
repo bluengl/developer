ac3a263816a647b8e6c616e8e3424ebb73e3b1dd
bluengl <ngl18896375227outlook.com>
2020-12-08 17:10:14 +0800

    delete

--- src/com/ngl/designer/authority/dao/AuthorityDao.java
+++
@@ -1,9 +0,0 @@
-package com.ngl.designer.authority.dao;
-
-import com.ngl.designer.authority.pojo.Role;
-
-import java.util.List;
-
-public interface AuthorityDao {
-    List<Role> selectRoleByLoginUserId(String loginUserId);
-}
